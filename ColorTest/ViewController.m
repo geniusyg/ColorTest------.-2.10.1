7293fb5697ba8a4cc8ac4ef26a17de1be2a28e47
Chae Yungi <l8673901@gmail.com>
2014-01-03 16:13:57 +0900

    ColorTest 실습입니다. 2.10.1
    ColorTest 실습입니다. 2.10.1

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,10 +9,45 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UISlider *greenSlider;
+@property (strong, nonatomic) IBOutlet UIView *colorView;
+@property (strong, nonatomic) IBOutlet UISlider *redSlider;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *indi;
+@property (strong, nonatomic) IBOutlet UISlider *blueSlider;
+
+- (IBAction)onColorSliderChanged:(id)sender;
 
 @end
 
 @implementation ViewController
+- (IBAction)powerOnOff:(id)sender {
+	BOOL isOn = ((UISwitch *)sender).on;
+	
+	_redSlider.enabled = isOn;
+	_greenSlider.enabled = isOn;
+	_blueSlider.enabled = isOn;
+	
+	if(isOn){
+		[_indi startAnimating];
+		
+		[self performSelector:@selector(onColorSliderChanged:) withObject:nil afterDelay:1.0];
+		
+	} else {
+		_colorView.backgroundColor = [UIColor grayColor];
+		_indi.hidesWhenStopped = YES;
+	}
+}
+
+- (IBAction)onColorSliderChanged:(id)sender {
+	if([_indi isAnimating]) {
+		[_indi stopAnimating];
+	}
+	_colorView.backgroundColor = [UIColor colorWithRed:_redSlider.value green:_greenSlider.value blue:_blueSlider.value alpha:1.0];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+	[self onColorSliderChanged:nil];
+}
 
 - (void)viewDidLoad
 {
